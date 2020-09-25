8d410818cd2091fb3a9da22301bf5be46ca2444c
takahirohana <w.hanasta.w@gmail.com>
2020-09-25 20:02:25 +0900

    ユーザ管理機能及びユーザー単体テストコード

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: (/@.+/) }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana,format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name_kana,format: { with: /\A[ァ-ン]+\z/ }
+  validates :birthday, presence: true
+end
