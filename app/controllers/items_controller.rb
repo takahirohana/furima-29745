4010af2ab3896b4c2626e799700bbf39f07e7134
takahirohana <w.hanasta.w@gmail.com>
2020-09-26 14:26:15 +0900

    active storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:user)
   end
   def new
-    @user = items.new
+    @item = Itme.new
   end
   def create
+    Item.create(itme_params)
   end
   def show
   end
   def edit
   end
+
+  private
+
+  def iteme_params
+    params.require(:item).permit(:image, :product_name, :sller, :category, :price, :product_status, :delivery_fee, :shipment_source, :shipping_days, :product_text).merge(user_id: current_user.id)
 end
