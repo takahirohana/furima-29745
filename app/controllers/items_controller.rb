175c1ddc98688ef7df65e6364018df4af2c2770a
takahirohana <w.hanasta.w@gmail.com>
2020-09-26 17:54:47 +0900

    コントローラーの記述変更　ログインしてるかどうかの判定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @items = Item.includes(:user)
   end
   def new
-    @item = Itme.new
+    @item = Item.new
   end
   def create
-    Item.create(itme_params)
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save #バリデーションをクリアした時
+      redirect_to root_path
+    else
+      render "new" #バリデーションで弾かれたとき
+    end
   end
+
   def show
   end
+
   def edit
   end
 
   private
 
-  def iteme_params
+  def item_params
     params.require(:item).permit(:image, :product_name, :sller, :category, :price, :product_status, :delivery_fee, :shipment_source, :shipping_days, :product_text).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :product_text
+    validates :category
+    validates :product_status
+    validates :delivery_fee
+    validates :shipment_source
+    validates :shipping_days
+    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :price, inclusion: {in: 300..9999999}
+  end
 end
