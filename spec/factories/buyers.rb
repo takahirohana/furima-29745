74ed9034fb43c2d72daf925e87a5028cab169414
takahirohana <w.hanasta.w@gmail.com>
2020-10-17 17:41:13 +0900

    微変更

--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :buyer do
-    token {"tok_abcdefghijk00000000000000000"}
+    token         { "tok_abcdefghijk00000000000000000" }
     postal_code   { "123-4567"}
     city          { "大阪府" }
     house_number  { "4-4-4" }
